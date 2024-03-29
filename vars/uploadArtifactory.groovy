3975105409096b3a97c9fa7caf9544b24ae0250b
Retesh-Gorai <66508093+Retesh-Gorai@users.noreply.github.com>
2024-03-29 15:02:35 +0530

    Update uploadArtifactory.groovy

--- vars/uploadArtifactory.groovy
+++ vars/uploadArtifactory.groovy
@@ -13,8 +13,8 @@ def execute() {
     sh "echo Jfrog cli has been installed successfully"
     sh "ls -ltR"
     // sh "chmod +x ${jfrogCliInstallationDir}/jfrog"
-    sh "echo $JFROG_CLI_HOME_DIR"
-    sh "jfrog --version"
+    // sh "echo $JFROG_CLI_HOME_DIR"
+    sh "jf --version"
     sh "echo Jfrog cli permission has been modified successfully"
 
     // Use Jenkins credentials to get Artifactory username and password
