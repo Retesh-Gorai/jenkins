24a9e721c78db40edd912d51f43440067b5dd30f
Retesh-Gorai <66508093+Retesh-Gorai@users.noreply.github.com>
2024-03-29 19:45:09 +0530

    Update uploadArtifactory.groovy

--- vars/uploadArtifactory.groovy
+++ vars/uploadArtifactory.groovy
@@ -1,32 +1,30 @@
 def execute() {
-    def jfrogCliUrl = "https://install-cli.jfrog.io"
+    def jfrogCliUrl = "https://getcli.jfrog.io"
     def jfrogCliVersion = "2"
     def jfrogCliInstallationDir = "/usr/local/bin"
     def targetJarFilePath = "../target/testJava.jar"
     def artifactoryUrl = "https://jcenter.bintray.com"
 
     //curl -fL https://getcli.jfrog.io/v2 | sh
-    // url -fL https://install-cli.jfrog.io | sh
+    //curl -fL https://install-cli.jfrog.io | sh
     // https://releases.jfrog.io/artifactory/jfrog-cli/v2/2.5.0/jfrog-cli-linux-amd64/
     // Download and install JFrog CLI
     // sh "chmod +wx ${jfrogCliInstallationDir}"
-    sh "curl -fL ${jfrogCliUrl}"
+    sh "curl -fL ${jfrogCliUrl}/v${jfrogCliVersion}"
     sh "echo Jfrog cli has been installed successfully"
     sh "ls -ltR"
-    sh "pwd"
-    sh "echo ${WORKSPACE}"
-    sh "chmod +x ${WORKSPACE}/jf"
+    sh "chmod +x ${WORKSPACE}/jfrog"
     sh "echo Jfrog cli permission has been modified successfully"
     // sh "echo $JFROG_CLI_HOME_DIR"
-    sh "${WORKSPACE}/jf --version"
+    sh "${WORKSPACE}/jfrog --version"
 
     // Use Jenkins credentials to get Artifactory username and password
     withCredentials([usernamePassword(credentialsId: 'jfrog-artifactory-cred', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         // Use JFrog CLI to upload the JAR file to Artifactory
         sh "echo Jfrog config has been initiated"
-        sh "${WORKSPACE}/jf rt config --url ${artifactoryUrl} --user ${USERNAME} --password ${PASSWORD} --interactive=false"
+        sh "${WORKSPACE}/jfrog rt config --url ${artifactoryUrl} --user ${USERNAME} --password ${PASSWORD} --interactive=false"
         sh "echo Jfrog config has been completed"
-        sh "${WORKSPACE}/jf rt u ${targetJarFilePath} ${artifactoryUrl}/testJava.jar"
+        sh "${WORKSPACE}/jfrog rt u ${targetJarFilePath} ${artifactoryUrl}/testJava.jar"
         sh "echo Jfrog upload has been completed"
     }
     // sh "echo Hi from retesh"
